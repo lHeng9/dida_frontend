e9ba5040097e995d7f19f0948a96d46a4da174eb
lHeng9 <770971581@qq.com>
2019-02-20 18:42:30 +0800

    lheng

---
+++ src/components/frontEnd/forum/forumIndex.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main">
+
+        <ForumBanner></ForumBanner>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .main{
+        /*border:1px solid red;*/
+        margin-top:38px;
+    }
+</style>
---
+++ src/components/frontEnd/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <h1>我真的是首页！！嘤嘤嘤~</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .main{
+        margin-top:38px;
+    }
+</style>
