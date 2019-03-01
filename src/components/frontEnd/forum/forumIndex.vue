bddab2bc9e2a4d2d70aa07365e3e0d7910c3f1c6
lHeng9 <770971581@qq.com>
2019-03-01 09:32:20 +0800

    lheng

--- src/components/frontEnd/forum/forumIndex.vue
+++ src/components/frontEnd/forum/forumIndex.vue
@@ -23,9 +23,7 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .main {
-        /*border:1px solid red;*/
         margin-top: 38px;
-        /*background-color:#000;*/
         background-color:#ddd;
 
     }
