56088e4cef84e300746491dee5d74d159384729b
XuAnQiNg_fd <924070845@qq.com>
2019-03-28 17:31:19 +0800

    接近完全体的面包屑以及，将用户管理的表头大换血，实现前端的模糊查询

--- src/components/afterEnd/userManage/userList.vue
+++ src/components/afterEnd/userManage/userList.vue
@@ -3,13 +3,67 @@
     <div>
         <h1>用户列表</h1>
         <div style="width: 100%;height: 1px; background-color: #909399; margin-bottom: 20px "></div>
-        <!--表格右侧悬停的栏-->
-        <a-table :columns="columns" :dataSource="data" :scroll="{ x: 1500, y: 470 }">
-            <a slot="banned" @click="showDeleteConfirm">封印</a>
-            <a slot="more" @click="showDrawer">更多</a>
+
+        <a-table :dataSource="data" :columns="columns">
+
+            <!--查询框-->
+            <div slot="filterDropdown"
+                 slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+                 class='custom-filter-dropdown'>
+                <!--出入要查询的内容-->
+                <a-input
+                        v-ant-ref="c => searchInput = c"
+                        :placeholder="`Search ${column.dataIndex}`"
+                        :value="selectedKeys[0]"
+                        @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+                        @pressEnter="() => handleSearch(selectedKeys, confirm)"
+                        style="width: 188px; margin-bottom: 8px; display: block;"
+                />
+                <!--探出的查询按钮-->
+                <a-button
+                        type='primary'
+                        @click="() => handleSearch(selectedKeys, confirm)"
+                        icon="search"
+                        size="small"
+                        style="width: 90px; margin-right: 8px"
+                >Search
+                </a-button>
+                <!--查询旁边的重置按钮-->
+                <a-button
+                        @click="() => handleReset(clearFilters)"
+                        size="small"
+                        style="width: 90px"
+                >Reset
+                </a-button>
+            </div>
+            <a-icon slot="filterIcon" slot-scope="filtered" type='search'
+                    :style="{ color: filtered ? '#108ee9' : undefined }"/>
+            <template slot="customRender" slot-scope="text">
+                <span v-if="searchText">
+                    <template
+                            v-for="(fragment, i) in text.toString().split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))">
+                        <mark v-if="fragment.toLowerCase() === searchText.toLowerCase()" :key="i"
+                              class="highlight">{{fragment}}</mark>
+                    <template v-else>{{fragment}}</template>
+                    </template>
+                </span>
+                <template v-else>{{text}}</template>
+            </template>
+            <!--<div slot="more" slot-scope="text">-->
+            <!--<a href="javascript; @click="showDrawer>more</a>-->
+            <!--<router-link :to="{name:''}"></router-link>-->
+            <!--</div>-->
+            <a slot="banned" slot-scope="text" @click="showDeleteConfirm">封印</a>
+            <a slot="more" slot-scope="text" @click="showDrawer">更多</a>
         </a-table>
+
         <!--右侧详情抽屉-->
-        <a-drawer width=50% placement="right" :closable="false" @close="onClose" :visible="visible">
+        <a-drawer
+                width=50%
+                placement="right"
+                :closable="false"
+                @close="onClose"
+                :visible="visible">
             <!--抽屉页面-->
             <div style=" margin: 0 auto">
                 <div style="width: 90% ; margin: 0 auto; ">
@@ -84,48 +138,98 @@
             </div>
 
         </a-drawer>
+
     </div>
 </template>
 <script>
+    const data = []
 
-    const columns = [
-        {title: 'ID', width: 100, dataIndex: 'number', key: 'number', fixed: 'left'},
-        {title: '姓名', width: 100, dataIndex: 'name', key: 'name'},
-        {title: '学院', dataIndex: 'college', key: 'college', width: 100},
-        {title: '班级', dataIndex: 'class', key: 'class', width: 130},
-        {title: '是否正常', dataIndex: 'status', key: 'status', width: 100},
-        {title: '加入时间', dataIndex: 'gmt_time', key: 'gmt_time', width: 100},
-        {title: '修改时间', dataIndex: 'gmt_modified', key: 'gmt_modified', width: 100},
-        {title: '是否违规', fixed: "right", width: 100, scopedSlots: {customRender: 'banned'},},
-        {title: '详情', fixed: 'right', width: 100, scopedSlots: {customRender: 'more'},},
-    ];
-
-    const data = [];
-    for (let i = 0; i <50; i++) {
+    for (let i = 0; i < 50; i++) {
         data.push({
             key: i,
             name: `人造人${i}号`,
-            number: `16140101${i}`,
-            college: '软件学院',
-            class: '16140Y02班',
-            status: '正常',
-            gmt_time: '2019-3-15',
-            gmt_modified: '2019-3-15',
+            age: `16140101${i}`,
+            address: '软件学院',
         });
     }
-
     export default {
         data() {
             return {
                 data,
-                columns,
+                searchText: '',
+                searchInput: null,
                 visible: false,
                 disabled: true, //决定输入框能否输入
-
+                columns: [
+                    {
+                        title: 'Name',
+                        dataIndex: 'name',
+                        key: 'name',
+                        scopedSlots: {
+                            filterDropdown: 'filterDropdown',
+                            filterIcon: 'filterIcon',
+                            customRender: 'customRender',
+                        },
+                        onFilter: (value, record) => record.name.toLowerCase().includes(value.toLowerCase()),
+                        onFilterDropdownVisibleChange: (visible) => {
+                            if (visible) {
+                                setTimeout(() => {
+                                    this.searchInput.focus()
+                                }, 0)
+                            }
+                        },
+                    },
+                    {
+                        title: 'Age',
+                        dataIndex: 'age',
+                        key: 'age',
+                        scopedSlots: {
+                            filterDropdown: 'filterDropdown',
+                            filterIcon: 'filterIcon',
+                            customRender: 'customRender',
+                        },
+                        onFilter: (value, record) => record.age.toLowerCase().includes(value.toLowerCase()),
+                        onFilterDropdownVisibleChange: (visible) => {
+                            if (visible) {
+                                setTimeout(() => {
+                                    this.searchInput.focus()
+                                })
+                            }
+                        },
+                    },
+                    {
+                        title: 'Address',
+                        dataIndex: 'address',
+                        key: 'address',
+                        scopedSlots: {
+                            filterDropdown: 'filterDropdown',
+                            filterIcon: 'filterIcon',
+                            customRender: 'customRender',
+                        },
+                        onFilter: (value, record) => record.address.toLowerCase().includes(value.toLowerCase()),
+                        onFilterDropdownVisibleChange: (visible) => {
+                            if (visible) {
+                                setTimeout(() => {
+                                    this.searchInput.focus()
+                                })
+                            }
+                        },
+                    },
+                    {title: '是否违规', dataIndex: 'banned',scopedSlots:{customRender:'banned'}},
+                    {title: '详情', dataIndex: 'more', scopedSlots:{customRender:'more'}},
+                ],
             }
         },
-        components: {},
         methods: {
+            handleSearch(selectedKeys, confirm) {
+                confirm()
+                this.searchText = selectedKeys[0]
+            },
+
+            handleReset(clearFilters) {
+                clearFilters()
+                this.searchText = ''
+            },
             Disabled() {
                 this.disabled = false
             },
@@ -156,8 +260,19 @@
         },
     }
 </script>
-
 <style scoped>
+    .custom-filter-dropdown {
+        padding: 8px;
+        border-radius: 4px;
+        background: #fff;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .15);
+    }
+
+    .highlight {
+        background-color: rgb(255, 192, 105);
+        padding: 0px;
+    }
+
     .a-input {
         background-color: rgba(255, 255, 255, 0.2);
         margin-bottom: 15px;
@@ -175,7 +290,6 @@
         margin: 20px auto;
         border-radius: 10%;
     }
-
     /*详细页修改按钮*/
     #update {
         margin-right: 20px;
